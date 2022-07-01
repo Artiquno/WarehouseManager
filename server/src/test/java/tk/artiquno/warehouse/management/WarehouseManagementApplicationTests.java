181da56bfed71a82592b3ac12a759839975ed80e
Artiquno <endri-kurushi@hotmail.com>
2022-07-01 21:46:46 +0200

    Integration tests should also work now
    They're still using MariaDB for now, this should be changed
    in another commit (hopefully...)

--- server/src/test/java/tk/artiquno/warehouse/management/WarehouseManagementApplicationTests.java
+++ server/src/test/java/tk/artiquno/warehouse/management/WarehouseManagementApplicationTests.java
@@ -1,13 +1,36 @@
 package tk.artiquno.warehouse.management;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.mockito.internal.util.collections.Iterables;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import tk.artiquno.warehouse.management.entities.Truck;
+import tk.artiquno.warehouse.management.repositories.TrucksRepo;
 
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 @SpringBootTest
 class WarehouseManagementApplicationTests {
 
-	@Test
-	void contextLoads() {
+	@Autowired
+	private TrucksRepo trucksRepo;
+
+	private Truck testingTruck;
+
+	@BeforeAll
+	public void addDataToDb() {
+		testingTruck = new Truck();
+		testingTruck.setLicensePlate("THANSR");
+		testingTruck.setChassis(42);
+		trucksRepo.save(testingTruck);
 	}
 
+	@Test
+	void contextLoads() {
+		// TODO: Use H2 for tests
+		Iterable<Truck> result = trucksRepo.findAll();
+		Assertions.assertEquals(Iterables.firstOf(result), testingTruck);
+	}
 }
