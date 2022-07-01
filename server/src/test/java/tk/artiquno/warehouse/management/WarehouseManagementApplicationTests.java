9120a446ceac951854c31e350dc95542b9ced38f
Artiquno <endri-kurushi@hotmail.com>
2022-07-01 12:29:01 +0200

    Initial commit

---
+++ server/src/test/java/tk/artiquno/warehouse/management/WarehouseManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package tk.artiquno.warehouse.management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WarehouseManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
